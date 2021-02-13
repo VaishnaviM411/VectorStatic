7d29943461e357f92bca0345340b6637044ab42b
Devang Kamble <risingentropy20@gmail.com>
2021-02-13 22:06:48 +0530

    putting temps

--- DeathEaters/DeathEaters/urls.py
+++ DeathEaters/DeathEaters/urls.py
@@ -5,7 +5,7 @@ from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('homePage/',views.home.as_view(),name = 'homepage'),
+    path('',views.home.as_view(),name = 'homepage'),
     path('loginPage/',views.Login.as_view(),name = 'Login'),
     path('logoutPage/',views.Logout.as_view(),name= 'Logout'),
 ]
