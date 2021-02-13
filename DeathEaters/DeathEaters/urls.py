4d0791c6e9e7e34c8a945613449c3ded5438f7dd
Devang Kamble <risingentropy20@gmail.com>
2021-02-14 00:01:15 +0530

    base html

--- DeathEaters/DeathEaters/urls.py
+++ DeathEaters/DeathEaters/urls.py
@@ -5,7 +5,7 @@ from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',views.home.as_view(),name = 'homepage'),
+    path('',views.home.as_view(),name = 'home'),
     path('loginPage/',views.Login.as_view(),name = 'Login'),
     path('logoutPage/',views.Logout.as_view(),name= 'Logout'),
     path('surveyorPage', views.surveyorPage.as_view(),name = 'surveyorPage'),
