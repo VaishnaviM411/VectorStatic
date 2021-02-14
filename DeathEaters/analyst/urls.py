6fffbf74a8cfff03e03df30aa5c061b0a5897eee
Devang Kamble <risingentropy20@gmail.com>
2021-02-14 08:56:08 +0530

    fml

--- DeathEaters/analyst/urls.py
+++ DeathEaters/analyst/urls.py
@@ -7,4 +7,6 @@ app_name = 'analyst'
 
 urlpatterns = [
     path('analystPage', views.analystPage.as_view(),name = 'analystPage'),
+    path('houseListQueries', views.houseListQueries.as_view(),name = 'houseListQueries'),
+    path('houseHoldQueries', views.houseHoldQueries.as_view(),name = 'houseHoldQueries'),
 ]
--- DeathEaters/analyst/views.py
+++ DeathEaters/analyst/views.py
@@ -1,6 +1,39 @@
 from django.shortcuts import render
 from django.views.generic.base import View
 # Create your views here.
+def isAnalyst(group_name):
+    if group_name == 'analyst_group':
+        return 1
+    else:
+        return 0
+
+
 class analystPage(View):
     def get(self, request):
-        return render(request, 'analystPage.html')
+        try:
+            group = request.user.groups.all()[0].name
+            if isAnalyst(group) == 0:
+                return render(request, 'login.html')
+            return render(request, 'analystPage.html')
+        except:
+            return render(request, 'login.html')
+
+class houseHoldQueries(View):
+    def get(self, request):
+        try:
+            group = request.user.groups.all()[0].name
+            if isAnalyst(group) == 0:
+                return render(request, 'login.html')
+            return render(request, 'houseHoldQueries.html')
+        except:
+            return render(request, 'login.html')
+
+class houseListQueries(View):
+    def get(self, request):
+        try:
+            group = request.user.groups.all()[0].name
+            if isAnalyst(group) == 0:
+                return render(request, 'login.html')
+            return render(request, 'houseListQueries.html')
+        except:
+            return render(request, 'login.html')
